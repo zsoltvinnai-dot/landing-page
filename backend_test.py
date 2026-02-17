8be274bbed9e0c81c42bf4637bae1863a9a75cce
Zsolt Vinnai <zsolt.vinnai@gmail.com>
2026-02-17 16:06:56 +0000

    auto-commit for 21d1d6f9-e335-4379-aaa3-747791e49b24

--- backend_test.py
+++ backend_test.py
@@ -26,6 +26,8 @@ class BeautyAPITester:
                 response = requests.get(url, headers=headers, timeout=10)
             elif method == 'POST':
                 response = requests.post(url, json=data, headers=headers, timeout=10)
+            elif method == 'DELETE':
+                response = requests.delete(url, headers=headers, timeout=10)
 
             success = response.status_code == expected_status
             
